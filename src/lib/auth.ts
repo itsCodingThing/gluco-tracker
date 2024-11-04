dea8d4c092cbfc8984c841f8644244e7fd65ccca
bhanu pratap singh <bhanuparitapsingh09@gmail.com>
2024-11-04 13:49:01 +0530

    added support for firebase emulator

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,17 @@
 import { app } from "./firebase";
-import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  connectAuthEmulator,
+} from "firebase/auth";
 import { storage } from "./storage";
 
 const auth = getAuth(app);
+
+if (import.meta.env.MODE === "development" && import.meta.env.DEV) {
+  connectAuthEmulator(auth, "http://127.0.0.1:9099", { disableWarnings: true });
+}
 
 export const authProvider = {
   async signin({ email, password }: { email: string; password: string }) {
